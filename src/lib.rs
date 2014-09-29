41565ffad84dd69aa2a1755e2ef25764266cf684
gong023 <gon.gong.gone@gmail.com>
2014-09-29 18:13:52 +0900

    change interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 #![crate_type="lib"]
 
-pub mod collection;
-
 pub struct Vect<T> {
     x: Vec<T>
 }
 
-impl<'a, T> Vect<T> {
+pub struct Iter;
+
+impl<'a, T: PartialEq + Clone> Vect<T> {
     pub fn new(v: Vec<T>) -> Vect<T> {
         Vect { x: v }
+    }
+
+    pub fn exists(x: &T, v: &Vec<T>) -> bool {
+        for element in v.iter() {
+            if element.eq(x) { return true; }
+        }
+
+        return false;
     }
 
     pub fn first(&'a self) -> &'a T {
@@ -40,11 +48,48 @@ impl<'a, T> Vect<T> {
         return rest;
     }
 
-    pub fn exists<T: PartialEq>(&'a self, x: &T) -> bool {
-        for element in &self.x.iter() {
-            if element.eq(x) { return true; }
+    pub fn without(&'a self, values: &Vec<T>) -> Vec<&'a T> {
+        let mut without_elements = Vec::new();
+        for element in self.x.iter() {
+            if ! Vect::exists(element, values) { without_elements.push(element) }
         }
 
-        return false;
+        return without_elements;
+    }
+
+    /// it should return &'a Vec<T>?
+    pub fn union(&'a self, adds: &Vec<T>) -> Vec<T> {
+        self.x.add(adds)
+    }
+
+    pub fn intersection(&'a self, intersec: &Vec<T>) -> Vec<&'a T> {
+        let mut intersected = Vec::new();
+        for element in self.x.iter() {
+            if Vect::exists(element, intersec) { intersected.push(element) }
+        }
+
+        return intersected;
+    }
+
+    pub fn uniq(&'a self) -> Vec<&'a T> {
+        let mut uniq = Vec::new();
+        for element in self.x.iter() {
+            if ! Vect::exists(&element, &uniq) { uniq.push(element) }
+        }
+
+        return uniq;
+    }
+}
+
+impl Iter {
+    pub fn each<'a, T>(self, v: &'a Vec<T>, func: |x: &'a T, index: uint|) {
+        let mut i = 0u;
+        loop {
+            match i <= v.len() {
+                true  => func(&v[i], i),
+                false => break
+            }
+            i += 1;
+        }
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -36,60 +36,38 @@ fn test_vec_rest() {
 
 #[test]
 fn test_vec_exists() {
-    let vec_int = vec!(1i, 2i, 3i);
-    assert!(__::Vect::new(vec_int).exists::<int>(&1i))
-//    assert!(__.exists::<int>(&1i, &vec_int));
-//
-//    let vec_str = vec!("aa", "bb", "cc");
-//    assert!(__.exists::<&str>(&"aa", &vec_str));
+    assert!(__::Vect::exists(&1i, &vec!(1i, 2, 3)));
 }
-//
-//#[test]
-//fn test_vec_without() {
-//    let __ = underscore::collection::Vect;
-//
-//    let vec_int = vec!(1i, 2i, 2i);
-//    let without_vec = __.without(&vec_int, &vec!(1i));
-//    for x in without_vec.iter() {
-//        assert_eq!(2i, **x);
-//    }
-//
-//    let vec_str = vec!("aa", "bb", "bb", "cc");
-//    let without_vec_str = __.without(&vec_str, &vec!("aa", "cc"));
-//    for x in without_vec_str.iter() {
-//        assert_eq!("bb", **x);
-//    }
-//}
-//
-//#[test]
-//fn test_vec_union() {
-//    let __ = underscore::collection::Vect;
-//
-//    let union_int_vec = __.union(&vec!(1i, 2i, 3i), &vec!(4i, 5i, 6i));
-//    assert_eq!(vec!(1i, 2, 3, 4, 5, 6), union_int_vec);
-//
-//    let union_str_vec = __.union(&vec!("aa", "bb"), &vec!("cc", "dd"));
-//    assert_eq!(vec!("aa", "bb", "cc", "dd"), union_str_vec);
-//}
-//
-//#[test]
-//fn test_vec_intersection() {
-//    let __ = underscore::collection::Vect;
-//
-//    let vec_int = vec!(1i, 2, 3);
-//    let intersect_int_vec = __.intersection(&vec_int, &vec!(2i, 4));
-//    for x in intersect_int_vec.iter() {
-//        assert_eq!(2i, **x);
-//    }
-//}
-//
-//#[test]
-//fn test_vec_uniq() {
-//    let __ = underscore::collection::Vect;
-//
-//    let vec_int = vec!(0u, 1, 1, 2, 2, 3);
-//    let uniq_int_vec = __.uniq(vec_int);
-//    for i in range(0u, 3) {
-//        assert_eq!(i, *uniq_int_vec.get(i));
-//    }
-//}
+
+#[test]
+fn test_vec_without() {
+    let vec_int = vec!(1i, 2i, 2i);
+    for x in __::Vect::new(vec_int).without(&vec!(1i)).iter() {
+        assert_eq!(2i, **x);
+    }
+
+    let vec_str = vec!("aa", "bb", "bb", "cc");
+    for x in __::Vect::new(vec_str).without(&vec!("bb", "cc")).iter() {
+        assert_eq!("aa", **x);
+    }
+}
+
+#[test]
+fn test_vec_union() {
+    let union_int_vec = __::Vect::new(vec!(1i, 2, 3)).union(&vec!(4i, 5i, 6i));
+    assert_eq!(vec!(1i, 2, 3, 4, 5, 6), union_int_vec);
+}
+
+#[test]
+fn test_vec_intersection() {
+    let intersect_int_vec = __::Vect::new(vec!(1i, 2, 3));
+    assert_eq!(1u, intersect_int_vec.intersection(&vec!(2i, 4)).len());
+    assert_eq!(2i, *intersect_int_vec.intersection(&vec!(2i, 4))[0]);
+}
+
+#[test]
+fn test_vec_uniq() {
+    let vec_int = __::Vect::new(vec!(0u, 1, 1, 2, 2, 3));
+    assert_eq!(0u, *vec_int.uniq()[0]);
+    assert_eq!(1u, *vec_int.uniq()[1]);
+}
